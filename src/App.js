38b65a01359fe9a1746ae866732594fa2e76d2f5
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-03-13 07:47:48 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[countries,setContries]=useState([])
+  console.log(countries)
+  useEffect(()=>{
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then(res=>res.json())
+    .then(data=>setContries(data))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+       <h2>{countries.length}</h2>
+       {countries.map(country=><li>{country.name} <img src={country.flag}></img></li> )}
+       
+      
     </div>
   );
 }
